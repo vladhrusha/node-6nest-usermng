a49a2d5eca8d73cd111131ba3b53d6eab578602f
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-07-11 20:30:07 +0300

    more cosmetic changes

--- subscribtion-bot-nestjs/src/app.module.ts
+++ subscribtion-bot-nestjs/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TelegrafModule } from 'nestjs-telegraf';
 import { BotModule } from './modules/bot/bot.module';
 
 @Module({
--- subscribtion-bot-nestjs/src/main.ts
+++ subscribtion-bot-nestjs/src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getBotToken } from 'nestjs-telegraf';
-const token = process.env.HOLIDAYBOT_TOKEN;
-const url = process.env.HEROKU_URL;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
