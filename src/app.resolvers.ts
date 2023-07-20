752dbc2989f667cfec885597303c42b75126f926
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-07-20 16:13:33 +0300

    test

--- src/app.resolvers.ts
+++ src/app.resolvers.ts
@@ -1,13 +1,12 @@
 import { AppService } from './app.service';
 
-import { Resolver, Mutation, Context, Query } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
 @Resolver()
 export class AppResolver {
   constructor(private appService: AppService) {}
   //root
   @Query(() => String)
   sayHelloMain(): string {
-    console.log('testlint');
     return 'Hello World!';
   }
 }
